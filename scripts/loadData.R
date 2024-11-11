eba8377d96df9a398b2856fb4db91468dadebc59
Antranig Basman <amb26ohloh@ponder.org.uk>
2024-11-11 16:13:43 +0000

    Fixed data references in loadData

--- scripts/loadData.R
+++ scripts/loadData.R
@@ -9,6 +9,8 @@ source("scripts/utils.R")
 originalSummary <- read.csv("tabular_data/Galiano_Tracheophyta_review_summary_reviewed_2024-10-07-assigned_revised.csv")
 filteredSummary <- read.csv("tabular_data/Galiano_Island_vascular_plant_records_consolidated-assigned-taxa.csv")
 
+records <- read.csv("tabular_data/Galiano_Island_vascular_plant_records_consolidated-prepared.csv")
+
 summary <- inner_join(x = originalSummary, y = filteredSummary, by = join_by(ID == id))
 
 # Subset historic, confirmed and new records
@@ -16,3 +18,4 @@ summary <- inner_join(x = originalSummary, y = filteredSummary, by = join_by(ID 
 new <- summary %>% filter(str_detect(Reporting.Status, "new"))
 confirmed <- summary %>% filter(Reporting.Status == "confirmed")
 reported <- summary %>% filter(Reporting.Status == "reported")
+observed <- summary %>% filter(Observation == 'observed') 
